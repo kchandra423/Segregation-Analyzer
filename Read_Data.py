aa85f389a019977add7512c8fdf992497a8d121f
Kumar.s.Chandra <kchandra423@student.fuhsd.org>
2021-11-29 14:10:02 -0800

    added some other infos

--- Read_Data.py
+++ Read_Data.py
@@ -1,4 +1,5 @@
 import json
+from pprint import pprint
 
 from Load_Data import BLACK, NATIVE, PACIFIC_ISLANDER, POPULATION
 
@@ -6,16 +7,27 @@ from Load_Data import BLACK, NATIVE, PACIFIC_ISLANDER, POPULATION
 def thing(state: str):
     with open(f'dataSets/{state}.json', 'r') as f:
         data = json.load(f)
-    print(calculate_dissimilarity(data['Counties'], get_brown_share(data), get_pop(data)))
+    dissimilarity = calculate_dissimilarity(data['Counties'], get_brown_share(data), get_pop(data))
+    print(dissimilarity)
+    print()
 
 
-def calculate_dissimilarity(sub_levels: list, brown_total_share: int, total_pop: int):
+def calculate_dissimilarity(sub_levels: list, brown_total_share: int, total_pop: int) -> int:
     index = 0
+    contributions = {}
     totals = total_pop * brown_total_share * (1 - brown_total_share)
     for sub_level in sub_levels:
-        index += get_pop(sub_level) * ((abs(get_brown_share(sub_level) - brown_total_share)) / totals)
+        contribution = 0.5 * get_pop(sub_level) * ((abs(get_brown_share(sub_level) - brown_total_share)) / totals) * 100
+        contributions[sub_level["NAME"]] = contribution
+        index += contribution
+    index = int(index)
+    for sub_level in sub_levels:
+        expected = sub_level[POPULATION] / total_pop
+        given = contributions[sub_level["NAME"]] / index
+        contributions[sub_level["NAME"]] = given/expected *100 -100
 
-    return int(index * 0.5 * 100)
+    pprint(contributions)
+    return index
 
 
 def get_pop(geo: dict) -> int:
